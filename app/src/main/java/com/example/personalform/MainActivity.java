ff806529023e37eb5c252251809a3537bdbd860a
DoQuocBinh <binhdq@fpt.edu.vn>
2023-10-09 15:19:56 +0700

    event handler

--- app/src/main/java/com/example/personalform/MainActivity.java
+++ app/src/main/java/com/example/personalform/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.personalform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,8 +30,28 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_spinner_item, yod);
         // Specify the layout to use when the list of choices appears.
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        
+
         // Apply the adapter to the spinner.
         spinner.setAdapter(adapter);
+
+        Button okButton = findViewById(R.id.button);
+        RadioButton maleRadio = findViewById(R.id.radioButton);
+        RadioButton femaleRadio = findViewById(R.id.radioButton2);
+        Spinner spinner1 = findViewById(R.id.spinner);
+        RadioGroup genderRadioGroup = findViewById(R.id.genderRadioGroup);
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               int selectGenderId = genderRadioGroup.getCheckedRadioButtonId();
+               RadioButton selectedGender= findViewById(selectGenderId);
+                Integer yod = (Integer) spinner1.getSelectedItem();
+                Toast.makeText(getApplication(),"You are " + selectedGender.getText()
+                        + yod,
+                        Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+
     }
 }
